0b1ecf001103e7abd7dcfae16a84d28fd776dc6b
soumitracode <psoumitra22@gmail.com>
2020-08-24 11:31:07 +0530

    update one record by id

--- router/index.js
+++ router/index.js
@@ -5,14 +5,14 @@ const mongo = require('mongodb')
 // inser single data to mongo db
 app.post('/insert', async (req, res) => {
   const data = req.body
-
+  console.log(req.body['go '])
   getDb()
     .then(con => {
       return con.production.collection('h999i').insertOne({ data })
     })
     .then(row => {
       console.log(row.result)
-      res.json({ status: row.result, data: row.ops })
+      res.json({ data: row.ops })
     })
     .catch(err => {
       res.end({ insert: false })
@@ -23,6 +23,7 @@ app.post('/insert', async (req, res) => {
 // fetching all inserted data
 app.get('/getall', async (req, res) => {
   getDb()
+  // for later implement query filtering
   // r => r.production.collection('h999i').find({}, { 'data.test': 1 }).toArray()
     .then(
       r => r.production.collection('h999i').find({}).toArray()
@@ -57,4 +58,36 @@ app.get('/getone/:id', (req, res) => {
     })
 })
 
+// update data
+app.put('/edit/:id', (req, res) => {
+  const id = new mongo.ObjectID(req.params.id)
+  const itest = req.body.test
+  // this notaition becaus i mistakenly add key with space
+  const igo = req.body['go ']
+  const ibuga = req.body.buga
+  getDb()
+    .then(
+      r => {
+        return r.production.collection('h999i').findOneAndUpdate(
+          { _id: id },
+          {
+            $set:
+            {
+              'data.test': itest,
+              // this notaition becaus i mistakenly add key with space
+              'data.go ': igo,
+              'data.buga': ibuga
+            }
+          },
+          { returnOriginal: false }
+        )
+      }
+    )
+    .then(
+      r => {
+        console.log(r.message)
+        res.send(r.value)
+      }
+    )
+})
 module.exports = app
