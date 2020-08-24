08cc272c4c67c6aaa8c13502274599be8ee55c02
soumitracode <psoumitra22@gmail.com>
2020-08-24 10:42:12 +0530

    single fetch by id and added proper response

--- router/index.js
+++ router/index.js
@@ -1,5 +1,6 @@
 const app = require('express').Router()
 const getDb = require('../DB')
+const mongo = require('mongodb')
 
 // inser single data to mongo db
 app.post('/insert', async (req, res) => {
@@ -14,6 +15,7 @@ app.post('/insert', async (req, res) => {
       res.json({ status: row.result, data: row.ops })
     })
     .catch(err => {
+      res.end({ insert: false })
       console.log(err)
     })
 })
@@ -21,6 +23,7 @@ app.post('/insert', async (req, res) => {
 // fetching all inserted data
 app.get('/getall', async (req, res) => {
   getDb()
+  // r => r.production.collection('h999i').find({}, { 'data.test': 1 }).toArray()
     .then(
       r => r.production.collection('h999i').find({}).toArray()
     )
@@ -29,6 +32,28 @@ app.get('/getall', async (req, res) => {
       res.send(r)
     })
     .catch(err => {
+      res.end({ fetch: false })
+      throw err
+    })
+})
+
+// find single data
+app.get('/getone/:id', (req, res) => {
+  const id = req.params.id
+  const mid = new mongo.ObjectID(id)
+  getDb()
+    .then(
+
+      r => r.production.collection('h999i').find({ _id: mid }).toArray()
+    )
+    .then(
+      r => {
+        console.log('get one', r)
+        res.send(r)
+      }
+    )
+    .catch(err => {
+      res.end({ singleFetch: false })
       throw err
     })
 })
